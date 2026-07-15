b28c3aaa1c40e57eb5f08f899bfecee58df2591a
OM <omjaiswal2552@gmail.com>
2026-07-15 23:51:27 +0530

    15 july

--- Node/httpServer.js
+++ Node/httpServer.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     // res.end('Response from Http Server');
-    if(req.url === '/') {
+    if(req.url === '/' && req.method === 'POST') {
         res.end('This is Home')
     } else if (req.url === '/login') {
         res.end('Response from Login')
