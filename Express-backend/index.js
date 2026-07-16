b67a075d88a6edf1f4f0da0d590592c44b249cda
OM <omjaiswal2552@gmail.com>
2026-07-16 22:55:39 +0530

    july 16

--- Express-backend/index.js
+++ Express-backend/index.js
@@ -1,24 +1,37 @@
 const express = require('express');
+const { logger } = require('./middlewares/loggers');
+// const morgan = require('morgan')
 
 const app = express();
+
+
+app.use(express.json());
+// app.use(morgan());
+
+// export let count = 0;
+
+app.use(logger);
+
 
 const students = [
     { id: 1, name: 'Tom', age: 22, course: 'mern' },
     { id: 2, name: 'John', age: 24, course: 'python'  },
     { id: 3, name: 'Harry', age: 23, course: 'cpp'  },
-    { id: 4, name: 'John', age: 26, course: 'java' }
+    { id: 4, name: 'John', age: 26, course: 'java' },
+    { id: 5, name: 'Virat', age: 24, course: 'mern' },
 ]
 
 app.get('/', (req, res) => {
     res.send('Response from server');
 });
 
+
 app.get('/login', (req, res) => {
     // res.status(201);
     res.send('Response from Login route')
 })
 
-app.get('/student/:id', (req, res) => {
+app.get('/students/:id', (req, res) => {
     const id = Number(req.params.id);
     // console.log('Id is: ',typeof(id));
     const student = students.find(s => s.id === id);
@@ -36,7 +49,7 @@ app.get('/student/:id', (req, res) => {
     return;
 })
 
-app.get('/student/:name/course/:course', (req, res) => {
+app.get('/students/:name/course/:course', (req, res) => {
     const name = req.params.name;
     const course = req.params.course;
 
@@ -55,6 +68,31 @@ app.get('/student/:name/course/:course', (req, res) => {
     return;
 })
 
+app.get('/search', (req, res) => {
+    const course = req.query.course;
+    const studentsData = students.filter(s => s.course === course);
+    if (studentsData) {
+        res.json(studentsData);
+        return;
+    }
+    res.json({message: 'Data not found'});
+    return;
+})
+
+
+app.post('/students', (req, res) => {
+    const data = req.body;
+    const newStudent = { id: students.length + 1, ...data };
+    students.push(newStudent);
+    res.json({ message: "New user added", NewUser: newStudent });
+})
+
+// app.put('/students/:id');
+
+// app.patch('/students/:id');
+
+// app.delete('/students/id');
+
 // app.post('/login', (req, res) => {
 //     res.status(202);
 //     res.send({ success: true, message: 'Logged in'});
@@ -62,8 +100,9 @@ app.get('/student/:name/course/:course', (req, res) => {
 
 // app.get('')
 
-app.post('/login', (req, res) => {
+app.post('/student', (req, res) => {
     res.status(201).json({ success: true, message: 'Logged in'});
+    
 })
 
 app.listen(3000);
